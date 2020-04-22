a0fdec506124af74d29b99579056f76ca5781adf
m1ke <git@m1ke.me>
2020-04-22 15:22:02 +0100

    Add var descriptions

--- variables.tf
+++ variables.tf
@@ -1,26 +1,23 @@
-# Probably the most useful variable, enter
-# the domain where you want your website hosted
 variable "domain_website" {
+  description = "Probably the most useful variable, enter the domain where you want your website hosted"
 }
-# Visit https://console.aws.amazon.com/route53/home#hosted-zones:
-# and look at the "Hosted Zone ID" column
 variable "route53_zone_id" {
+  description = "Visit https://console.aws.amazon.com/route53/home#hosted-zones: and look at the \"Hosted Zone ID\" column"
 }
 
-# Can be anything the user wants
 variable "bucket_website_name" {
   default = "Static website hosting bucket"
+  description = "Tag Name for the website bucket"
 }
 variable "bucket_logs_name" {
   default = "Static website logs bucket"
+  description = "Tag Name for the logs bucket"
 }
-# Set this to 0 to disable caching or
-# higher to increase cache time
 variable "ttl" {
   default = 300
+  description = "Set this to 0 to disable caching or higher to increase cache time"
 }
-
-# Can be set if desired
 variable "cert_name" {
   default = "Static website certificate"
+  description = "Name of the ACM certificate"
 }
